d42277fb2ddf6ae2f3bcc86445bb415afa04f0a9
Your Name <you@example.com>
2022-04-05 09:48:31 +0700

    btap mang

---
+++ module2/p_module2/src/Bai3/BaiTap/GopMang.java
@@ -0,0 +1,12 @@
+package Bai3.BaiTap;
+
+import java.util.Scanner;
+
+public class GopMang {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr1=
+
+
+    }
+}
---
+++ module2/p_module2/src/Bai3/BaiTap/TimPhanTuLonNhatTrongMang2Chieu.java
@@ -0,0 +1,4 @@
+package Bai3.BaiTap;
+
+public class TimPhanTuLonNhatTrongMang2Chieu {
+}
