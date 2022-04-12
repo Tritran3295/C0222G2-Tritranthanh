33cb107c1d91070d0be2afb4e14ba8f70d1225cc
Your Name <you@example.com>
2022-04-12 20:37:57 +0700

    update

--- module2/p_module2/src/ss7_abtract_class_and_interface/BaiTap/Bai2/Main.java
+++ module2/p_module2/src/ss7_abtract_class_and_interface/BaiTap/Bai2/Main.java
@@ -7,13 +7,20 @@ import ss7_abtract_class_and_interface.BaiTap.Bai1.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Shape []shape = new Shape[3];
-        shape[0]=new Circle(5,"blue",true);
-        shape[1]=new Square(6,"red",true);
-        shape[2]=new Rectangle(5,5,"white",true);
-        for (Shape a:shape) {
-            
-        }
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Circle(5);
+        shapes[1] = new Square(6);
+        shapes[2] = new Rectangle(5, 5);
+        for (Shape shape : shapes) {
+            if (shape instanceof Rectangle) {
+                System.out.println("Area of the Rectangle is:" + ((Rectangle) shape).getArea());
+            }if(shape instanceof Circle){
+                System.out.println("Area of the Circle is:"+ ((Circle)shape).getArea());
+            }if(shape instanceof Square){
+                System.out.println("Area of the Square is :"+((Square)shape).getArea());
+                ((Square)shape).howToColor();
+            }
 
+        }
     }
 }
